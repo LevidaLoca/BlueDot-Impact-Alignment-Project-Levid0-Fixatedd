825d8b6a7975e274290363e0197681766df3b5ea
Fixated Pc <fixatedDev@example.com>
2025-02-02 01:51:46 +0000

    added generate_async_response to api_client.py to allow asynchronous llm calls

--- src/api_client.py
+++ src/api_client.py
@@ -1,4 +1,5 @@
 from huggingface_hub import InferenceClient
+from huggingface_hub import AsyncInferenceClient
 from dotenv import load_dotenv
 import os
 
@@ -20,6 +21,27 @@ def generate_response(prompt, max_length=None):
         prompt,
         model="google/gemma-2-9B-it",
         **params
+    )
+    
+    return response
+
+
+async def generate_response_async(prompt,max_length=None):
+    client = AsyncInferenceClient(token=os.getenv("HUGGINGFACE_TOKEN"))
+    
+    # Default model parameters
+    max_new_tokens = max_length 
+    
+    params = {
+        "max_new_tokens": max_new_tokens,
+        "temperature": 0.7,
+        "do_sample": True
+    }
+
+    response = await client.text_generation(
+        prompt,
+        model="google/gemma-2-9B-it",
+        **params 
     )
     
     return response
